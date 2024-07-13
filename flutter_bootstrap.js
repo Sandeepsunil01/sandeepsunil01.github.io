4dc9ff54d8d8a571d3058f58f1048b340d010bf9
sandeep-kiran <sandeep.kiran@global-csg.com>
2024-07-13 22:56:56 +0530

    Bug Fixes and Some UI Changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "20943472"
+    serviceWorkerVersion: "3117429834"
   }
 });
